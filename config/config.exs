6c052bde8d1c4fe10abc33efc4db36c494b8b1d7
Luan Pontes <luanpontes2@gmail.com>
2019-12-05 02:33:04 -0300

    adicionar guard configs

--- config/config.exs
+++ config/config.exs
@@ -27,6 +27,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Guardian config
+config :bankapi, Bank.Guardian,
+       issuer: "bankapi",
+       secret_key: "u42lmqps7xcVl5FSfwa985zht1EaKO3/6BtMY3HJtH34wvAlEhclnqKm11Xbm5MN"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
