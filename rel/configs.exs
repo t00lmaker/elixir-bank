97f38aea7b91946337c2e71d671db5ec992baf77
luan <luan@infoway-pi.com.br>
2019-12-10 08:20:12 -0300

    nova estrategia para subir release

---
+++ rel/configs.exs
@@ -0,0 +1,9 @@
+release :apibank do
+  # snip..
+  set config_providers: [
+    {Distillery.Releases.Config.Providers.Elixir, ["${RELEASE_ROOT_DIR}/etc/config.exs"]}
+  ]
+  set overlays: [
+    {:copy, "rel/config/config.exs", "etc/config.exs"}
+  ]
+end
