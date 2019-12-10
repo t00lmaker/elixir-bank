176a6015686690f08b99aa96dffe5aac2bb14146
luan <luan@infoway-pi.com.br>
2019-12-09 22:09:10 -0300

    remove exceptions ao startar a aplicação

--- config/releases.exs
+++ config/releases.exs
@@ -5,11 +5,7 @@
 import Config
 
 database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+  System.get_env("DATABASE_URL")
 
 config :bankapi, Bank.Repo,
   ssl: true,
@@ -17,11 +13,7 @@ config :bankapi, Bank.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+  System.get_env("SECRET_KEY_BASE")
 
 config :bankapi, BankWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
